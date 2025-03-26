2ff4e0b8be66c4d3a85f5e081e917f0b09379faf
nigabeto <joserpinto@gmail.com>
2025-03-26 11:08:24 -0300

    Implementaçao do main_menu.xml para inclusao da função Log out

--- app/src/main/java/com/jrmedia/rosafoods/HomeActivity.java
+++ app/src/main/java/com/jrmedia/rosafoods/HomeActivity.java
@@ -2,11 +2,15 @@ package com.jrmedia.rosafoods;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -18,14 +22,19 @@ import com.jrmedia.rosafoods.fragment.HomeFragment;
 
 public class HomeActivity extends AppCompatActivity {
     Fragment homeFragment;
+    private Toolbar mToolbar;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         /*EdgeToEdge.enable(this);*/
         setContentView(R.layout.activity_home);
-        homeFragment=new HomeFragment();
+        homeFragment = new HomeFragment();
         loadFragment(homeFragment);
+        mAuth=FirebaseAuth.getInstance();
+        mToolbar = findViewById(R.id.home_toolbar);
+        setSupportActionBar(mToolbar);
 
         /*Button btn=findViewById(R.id.logout);
         btn.setOnClickListener(new View.OnClickListener() {
@@ -44,10 +53,28 @@ public class HomeActivity extends AppCompatActivity {
             return insets;
         });*/
     }
+
     private void loadFragment(Fragment fragment) {
-        FragmentTransaction transaction=getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.home_container,homeFragment);
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.home_container, homeFragment);
         transaction.addToBackStack(null);
         transaction.commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.logout_btn) {
+            mAuth.signOut();
+            Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        return true;
     }
 }
